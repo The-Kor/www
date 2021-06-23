b4ac6ddc07b3d80e347713ae0da61a4c956ee5e6
koralh <koralh@cs.huji.ac.il>
2021-06-23 16:09:50 +0300

    #36 - Implemented a decorator which fills in the soup object of the Result instance only if it wasn't initialized before. Implementing it as a decorator makes the initialization lazy and saves some run time as the request to the target page is dont only when it's data is needed (when calling get_title or get_thread).

--- result.py
+++ result.py
@@ -1,17 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def init_soup_obj_decor(func):
+    def wrapper(*args):
+        self = args[0]
+        if self.page is None or self.soup_obj is None:
+            self.page = requests.get(self.link)
+            self.soup_obj = BeautifulSoup(self.page.content, 'html.parser')
+        return func(*args)
+
+    return wrapper
+
+
 class Result:
     def __init__(self, link, parser_class):
         self.link = link
         self.parser = parser_class
-        self.page_content = None
+        self.page = None
+        self.soup_obj = None
         self.title = ""
         self.thread = None
 
+    @init_soup_obj_decor
     def get_title(self):
         # Fill page_content if None
         # Calls self.parser parse title, is self.title is "" , returns self.title
-        pass
+        if not self.title:
+            self.title = self.parser.parse_title(self.soup_obj).split("\n\n")[0]
+        return self.title
 
+    @init_soup_obj_decor
     def get_thread(self):
         # Fill page_content if None
         # Calls self.parser parse link if self.thread is None, retuns self.thread
-        pass
+        if not self.thread:
+            self.thread = self.parser.parse_thread(self.soup_obj, self.link)
+        return self.thread
